6f7320768d4e0f464c8d93dc0817ad5fb0584649
abrahamlaria <abraham@abrahamlaria.com>
2018-02-23 09:51:01 -0800

    First commit

---
+++ FirstDuplicate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstDuplicate
+{
+    class Program
+    {
+        public static int FirstDuplicate(int[] a)
+        {
+
+            HashSet<int> set = new HashSet<int>();
+
+            for(var i = 0; i <= a.Length - 1; i++)
+            {
+                if (set.Contains(a[i]))
+                {
+                    return a[i];
+                }
+
+                set.Add(a[i]);
+            }
+
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = {2, 3, 3, 1, 5, 2};
+            Console.Write(FirstDuplicate(arr));
+            Console.ReadLine();
+
+        }
+    }
+}
